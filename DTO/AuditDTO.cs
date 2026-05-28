548d879210a1d46e06c13132f2968aaa31aaa4bd
Hariteja <2499243@cognizant.com>
2026-05-28 10:43:57 +0530

    Created AuditDTO

---
+++ DTO/AuditDTO.cs
@@ -0,0 +1,7 @@
+public class AuditDTO
+{
+    public int Id { get; set; }
+    public string Action { get; set; }
+    public string User { get; set; }
+    public DateTime Timestamp { get; set; }
+}
